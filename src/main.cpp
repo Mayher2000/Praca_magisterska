998f78676efe0b4aec93d26c2ff8ec63da9dd698
Mayher2000 <jmajcher2000@gmail.com>
2024-02-29 17:08:45 +0100

    4 antenna prototype to be tested

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ const byte RS_demux_G = 16; // demux enable
 // GLOBAL VARIABLES 
 /////////////////////////////////////////////////////
 bool twoAntennaFlag = true;
-
+byte activeAntenna = 1;
 /////////////////////////////////////////////////////
 // GLOBAL INSTANCES 
 /////////////////////////////////////////////////////
@@ -35,7 +35,8 @@ MFRC522 rfid(SS1_1, RS_demux_G); //MFRC522 instance
 /////////////////////////////////////////////////////
 void dump_byte_array(byte * buffer, byte bufferSize);
 void pinModeSetup();
-void AntennaTest();
+void AntennaTestInit();
+void AntennaTestRead();
 
 
 
@@ -68,10 +69,60 @@ void setup() {
 /////////////////////////////////////////////////////
 
 void loop() {
-  AntennaTest();
+  AntennaTestRead();
 }
 
-void AntennaTest() {
+void AntennaTestRead() {
+  // State machine for antennas switching
+  switch (activeAntenna){
+  case 1:
+    AntennaSelect(SS1_1, 0, 0, 0, 1);
+    break;
+  case 2:
+    AntennaSelect(SS1_2, 0, 0, 0, 1);
+    break;
+  case 3:
+    AntennaSelect(SS1_1, 1, 0, 0, 1);
+    break;
+  case 4:
+    AntennaSelect(SS1_2, 1, 0, 0, 1);
+    break;
+  default:
+    Serial.println("Invalid antenna number");
+    break;
+  }
+  // Initing the Antenna 
+  rfid.PCD_Init();
+
+  // Looking for a card
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    Serial.print("|\t");
+    rfid.PICC_DumpDetailsToSerial(&(rfid.uid)); //dump some details about the card
+    Serial.print("\t|");
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();  
+  }
+  else Serial.println("|\tNONE\t|");
+  if (activeAntenna % 2 == 1) Serial.println();
+  delay(100);
+}
+
+void AntennaSelect(byte SS, byte RS_A, byte RS_B, byte RS_C, int selectedAntenna) {
+    // set SS for Antenna 1
+    rfid.PCD_ChangeChipSelectPin(SS);
+    // set RST for Antenna 1
+    digitalWrite(RS_demux_A, RS_A);
+    digitalWrite(RS_demux_B, RS_B);
+    digitalWrite(RS_demux_C, RS_C);
+    // debug info
+    Serial.print("Ant");
+    Serial.print(selectedAntenna);
+    Serial.print(" :");
+    // change the state for next iteration
+    if (activeAntenna == 4) activeAntenna = 1;
+    else activeAntenna = selectedAntenna + 1;
+}
+void AntennaTestInit() {
   // Init PCD
   
   if (twoAntennaFlag == true){
